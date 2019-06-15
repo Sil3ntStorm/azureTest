f38862877a75b8d100bee0d21f80cb709a37201a
Sil3ntStorm <Sil3ntStorm@users.noreply.github.com>
2019-06-16 00:37:17 +0200

    Inline PowerShell Script

--- ci/download_qt.ps1
+++
@@ -1,8 +0,0 @@
-$Wc = New-Object System.Net.WebClient
-Write-Host "Downloading Qt..."
-try {
-	$Wc.DownloadFile($env:QT_SOURCE_URL, ${env:QTFILE})
-}
-catch {
-	Write-Host $error[0].Exception.ToString()
-}
